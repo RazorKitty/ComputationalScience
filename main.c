4df3fb0de69dbe0304cb9932cabbfd4530244f71
RazorrKitty <razorkitty@null.net>
2019-03-12 17:52:39 +0000

    removed white space and pointess brakets

--- main.c
+++ main.c
@@ -13,17 +13,15 @@ int main(int argc, char **argv) {
     int k = 0;
     int samples = 0;
     int sampleSize = 10;
-
     FILE *f = fopen("output.txt", "w");
-
     while (T <= 15) { 
         u = (T < 5) ? 2.0 : (T < 10) ? 1.0 : 3.0;
         x[k + 1] = x[k] + H * (a * x[k] + b * u);
-        if (++samples % sampleSize == 0) {
+        if (++samples % sampleSize == 0)
             fprintf(f, "T=%.6f\tx[k]=%.6f\tU=%.6f\n", T, x[k], u);
-        }
         T += H;
         ++k;
     }
+    fclose(f);
     return 0;
 }
