f028206c77f32dd3a3831226594d8edeff7a8ced
RazorrKitty <razorkitty@null.net>
2019-03-13 13:07:37 +0000

    clean up

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int main(int argc, char **argv) {
     double H = 0.01;
 	double T0 = 0.0;
 	double Tf = 15.0;
-	double T = 0.00;
 	double n = Tf - T0 / H;
 	double a = -2.0;
 	double b = 2.0;
@@ -14,12 +14,11 @@ int main(int argc, char **argv) {
     int samples = 0;
     int sampleSize = 10;
     FILE *f = fopen("output.txt", "w");
-    while (T <= 15) { 
+    for (double T = 0; T < Tf; T += H) {
         u = (T < 5) ? 2.0 : (T < 10) ? 1.0 : 3.0;
         x[k + 1] = x[k] + H * (a * x[k] + b * u);
         if (++samples % sampleSize == 0)
             fprintf(f, "T=%.6f\tx[k]=%.6f\tU=%.6f\n", T, x[k], u);
-        T += H;
         ++k;
     }
     fclose(f);
