3d27a7450551cc168d6573d27b7c2a8c4d72726e
RazorrKitty <razorkitty@null.net>
2019-03-14 18:13:36 +0000

    made changes

--- main.c
+++ main.c
@@ -13,14 +13,17 @@ int main(int argc, char **argv) {
     int k = 0;
     int samples = 0;
     int sampleSize = 10;
-    FILE *f = fopen("output.txt", "w");
-    for (double T = 0; T < Tf; T += H) {
+    FILE *file1 = fopen("output.txt", "w");
+    for (double T = 0; T < Tf; T += H, ++k) {
         u = (T < 5) ? 2.0 : (T < 10) ? 1.0 : 3.0;
         x[k + 1] = x[k] + H * (a * x[k] + b * u);
         if (++samples % sampleSize == 0)
-            fprintf(f, "T=%.6f\tx[k]=%.6f\tU=%.6f\n", T, x[k], u);
-        ++k;
+            fprintf(file1, "T=%.6f\tx[k]=%.6f\tU=%.6f\n", T, x[k], u);
     }
-    fclose(f);
+    fclose(file1);
+    *file1 = fopen("ouput.txt", "r");
+    FILE *file2 = fopen("noisy.csv", "w");
+
+
     return 0;
 }
